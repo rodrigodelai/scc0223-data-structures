23c6f40199fc9fdd480140a5816833e0225d0c94
Rodrigo Delai <rodrigo-delai@hotmail.com>
2022-10-19 21:18:23 -0300

    Inclui novos codigos de erro

--- ex4/util.h
+++ ex4/util.h
@@ -9,11 +9,14 @@ typedef int boolean;
 #define TRUE 1
 #define FALSE 0
 #define MAX 100
-#define ERRO_NULL -32
+#define ERRO_MEM -300    // Ocorre quando uma alocacao de momoria falha;
+#define ERRO_SIM -301    // Ocorre quando uma funcao recebe como parametro um simbolo invalido (Simbolo != {1,2,...,9,10,V,D,R});
+#define ERRO_NUM -302    // Ocorre quando uma funcao recebe como parametro um numero invalido;
+#define ERRO_NULL -303   // Ocorre quando uma funcao recebe como parametro um ponteiro que aponta para NULL;
 
 char* ler_palavra();
-void exibir_palavra(char* linha);
-boolean apagar_palavra(char** linha);
+void exibir_palavra(char* palavra);
+boolean apagar_palavra(char** palavra);
 void exibir_booleano();
 
 #endif // UTIL_H
