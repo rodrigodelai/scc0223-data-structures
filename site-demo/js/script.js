f166c4be459d37ea3f883ea906ccf26a6da8269b
Rodrigo Delai <rodrigo-delai@hotmail.com>
2023-08-13 22:54:17 -0300

    Adiciona transicoes e carregamento sob demanda

--- site-demo/js/script.js
+++ site-demo/js/script.js
@@ -1,33 +1,70 @@
+let displayedContent;
+let titles = document.querySelectorAll(".title");
+let contents = document.querySelectorAll(".content-cards");
+let iframes = document.querySelectorAll("iframe");
+let iframeLinks = ["https://replit.com/@delai/ex1-pontos-cartesianos#main.c", 
+                   "https://replit.com/@delai/ex2-catalogo-de-jogos#main.c",
+                   "",
+                   "https://replit.com/@delai/ex4-pilha-de-cartas#main.c",
+                   "https://replit.com/@delai/ex5-big-numbers#main.c",
+                   "https://replit.com/@delai/ex6-conta-bancaria#main.c",
+                   "",
+                   "https://replit.com/@delai/ex8-catalogo-de-jogos-ii#main.c"];
+
+loadIframeContent(1);
+showSelectedContent(1);
 addClickEventToButtons();
 
-function addClickEventToButtons(){
-    let buttons = document.querySelectorAll("nav li");
+function addClickEventToButtons() {
+    let buttons = document.querySelectorAll("header li");
 
-    console.log(buttons);
-
-    for (let i=0; i<buttons.length; i++){
+    for (let i=0; i<buttons.length; i++) {
         buttons[i].onclick = handleButtonClick;
-        buttons[i].contentReference = "ex" + (i+1);
+        buttons[i].contentReference = i + 1;
     }
 }
 
-function handleButtonClick(event){
-    hideAllContents();
-    showProperContent(event.target.contentReference);
+function handleButtonClick(event) {
+    hideDisplayedContent();
+    loadIframeContent(event.target.contentReference);
+    showSelectedContent(event.target.contentReference);
 }
 
-function hideAllContents(){
-    let contents = document.querySelectorAll(".content-cards");
-    let titles = document.querySelectorAll(".title");
+function hideDisplayedContent() {
+    iframes[displayedContent - 1].style.opacity = 0;
+    titles[displayedContent - 1].style.opacity = 0;
+    contents[displayedContent - 1].style.opacity = 0;
 
-    for (let i=0; i<contents.length; i++){
-        contents[i].style.display = "none";
-        titles[i].style.display = "none";
+    setTimeout(() => {
+        iframes[displayedContent - 1].style.display = "none";
+        titles[displayedContent - 1].style.display = "none"
+        contents[displayedContent - 1].style.display = "none";
+    }, 400)
+}
+
+function loadIframeContent(selectedContent) {
+    let iframe = iframes[selectedContent - 1];
+
+    if (!iframe.wasLoaded) {
+        iframe.src = iframeLinks[(selectedContent - 1)];
+        iframe.wasLoaded = true;
     }
 }
 
-function showProperContent(className){
-    let content = document.getElementsByClassName(className);
-    content[0].style.display = "flex";
-    content[1].style.display = "block";
+function showSelectedContent(selectedContent) {
+    setTimeout(() => {
+        iframes[selectedContent - 1].style.display = "block";
+        titles[selectedContent - 1].style.display = "flex";
+        contents[selectedContent - 1].style.display = "block";
+    }, 450);
+
+    setTimeout(() => {
+        iframes[selectedContent - 1].style.opacity = 100;  
+        titles[selectedContent - 1].style.opacity = 100;
+        contents[selectedContent - 1].style.opacity = 100;  
+    }, 500);
+
+    setTimeout(() => {
+        displayedContent = selectedContent;
+    }, 550);
 }
